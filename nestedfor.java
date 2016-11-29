641fe723f34c4b63fab9ee85e85858c585976e99
ramanakumarvavilla <ramanakumarvavilla@gmail.com>
2016-11-29 22:04:24 +0530

    Add files via upload

---
+++ nestedfor.java
@@ -0,0 +1,23 @@
+package exampleforloop;
+
+public class Forloopexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<3;i++)
+		{
+		for(int j=0;j<3;j++)
+		{
+			
+			System.out.println("chennai");
+			}
+		
+			System.out.println("bangalore");
+				System.out.println("goa");
+		}
+		
+		
+	
+	}
+
+}
