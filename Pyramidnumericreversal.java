cdf95fcf4b058c000c177af218b224482b70b0f7
ramanakumarvavilla <ramanakumarvavilla@gmail.com>
2016-12-03 00:29:46 +0530

    Add files via upload

---
+++ Pyramidnumericreversal.java
@@ -0,0 +1,17 @@
+ package numberreversal;
+public class Pyramidnumericreversal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int i=8;i>=0;i--)
+{
+	for (int j=0;j<=i;j++)
+	{
+		System.out.print(i+1);
+		}
+		
+		System.out.println("");
+	}
+
+}
+}
