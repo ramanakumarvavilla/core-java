a1a603ffc1a28c43c922a785fe7ada6362c75af9
ramanakumarvavilla <ramanakumarvavilla@gmail.com>
2016-12-06 16:15:25 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,21 @@
+package array1;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c[]=new int[7];
+		int i;
+		c[0]=90;
+		c[1]=70;
+		c[2]=85;
+		c[3]=45;
+		c[4]=30;
+		c[5]=72;
+		c[6]=213;
+		for(i=0;i<=6;i++)
+			System.out.println(c[i]);
+
+	}
+
+}
