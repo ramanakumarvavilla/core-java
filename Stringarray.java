22fd3c980f5570752a212785cde725d38ade3866
ramanakumarvavilla <ramanakumarvavilla@gmail.com>
2016-12-06 16:36:03 +0530

    Add files via upload

---
+++ Stringarray.java
@@ -0,0 +1,19 @@
+package array1;
+
+public class Stringarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String  z[]=new String[5];
+	    int i=4;
+		z[0]="i";
+		z[1]=" am";
+		z[2]=" a";
+		z[3]=" java";
+		z[4]=" developer";
+		z[i]=z[0]+z[1]+z[2]+z[3]+z[4];
+		System.out.print(z[i]);
+
+	}
+
+}
