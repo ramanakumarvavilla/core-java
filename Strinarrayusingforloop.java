a4bf535616d8108265e7dee83d21a676f7953026
ramanakumarvavilla <ramanakumarvavilla@gmail.com>
2016-12-06 16:49:50 +0530

    Add files via upload

---
+++ Strinarrayusingforloop.java
@@ -0,0 +1,23 @@
+package stringarray1;
+
+public class Strinarrayusingforloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String  v[]=new String[11];
+		v[0]="I";
+		v[1]="will";
+		v[2]="be";
+		v[3]="one";
+		v[4]="of";
+		v[5]="the";
+		v[6]="best";
+		v[7]="person";
+		v[8]="in";
+		v[9]="the";
+		v[10]="world";
+		for(int i=0;i<=10;i++)
+			System.out.println(v[i]);
+	}
+
+}
